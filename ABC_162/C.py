018a4d0964782f0db5b5f4b211e0158a5d71e494
Yoshi <OnsenTamagoYoshi@users.noreply.github.com>
2020-05-28 04:50:09 +0900

    AC

--- ABC_162/C.py
+++ ABC_162/C.py
@@ -1,11 +1,22 @@
-import numpy as np
+# 参考
+# https://note.com/tanon_cp/n/n2eb2bb1d69f9#OH0EO
+import math
+import collections
 
 K = int(input())
 
-total = 0
+# intで初期化するため0が返る
+cnt = collections.defaultdict(int)
 for i in range(1, K + 1):
     for j in range(1, K + 1):
-        for k in range(1, K + 1):
-            total = total + np.gcd.reduce([i, j, k])
+        key = math.gcd(i, j)
+        # 最大公約数をkeyにし、同じ最大公約数の出現回数を保持
+        cnt[key] = cnt[key] + 1
+
+total = 0
+for k in range(1, K + 1):
+    for key in cnt.keys():
+        # 出現済みの最大公約数との最大公約数を取り、出現回数分を加算する
+        total = total + math.gcd(k, key) * cnt[key]
 
 print(total)
